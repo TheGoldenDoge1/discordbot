863defd1fd88800147a6fb03fcd5abd8de96a76a
thegoldendoge1 <thegoldendoge1@gmail.com>
2023-05-26 21:39:12 +0700

    added youtube url's support for 'play' command

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import discord
 from discord import app_commands
 from discord.ext import commands
-import youtube_dl
+import yt_dlp as youtube_dl
+import asyncio
 
 # Токены
-token = ''
+token = 'ODI4MTQ5Njg0MTIzMzM2NzM0.G3Q-V-.UWYPjLsPz3tn6k8WoMXA4PQVpRrM_9FVQ3UkQ8'
 
 # Все разрешения интентов
 intents = discord.Intents.all()
@@ -19,28 +20,43 @@ async def on_ready():
         print(f'** synced {len(synced)} command(s)')
     except Exception as e:
         print(e)
-@bot.tree.command(name='test')
+@bot.tree.command(name='test', description='only for developers ^_^')
 async def hello(interaction: discord.Interaction):
     await interaction.response.send_message(f'test command succesfully! {interaction.user.mention}')
 
-@bot.tree.command(name='play')
-async def play(interaction: discord.Interaction, url: str):
-    ffmpeg_path = r'C:\Users\thegoldendoge\Downloads\ffmpeg-2023-05-22-git-877ccaf776-full_build\bin\ffmpeg.exe'
-    channel = interaction.user.voice.channel
-    voice_client = interaction.guild.voice_client
-    if voice_client is None:
-        voice_client = await channel.connect()
-    else:
-        await voice_client.move_to(channel)
-
-    audio_source = discord.FFmpegPCMAudio(url, executable=ffmpeg_path)
-    voice_client.play(audio_source)
-
-    await interaction.response.send_message('Playing song from url')
 @bot.tree.command(name='stop', description='Stop playing current song')
 async def stop(interaction: discord.Interaction):
     voice_client = interaction.guild.voice_client
     await voice_client.disconnect()
-    await interaction.response.send_message('Stopped and disconnected from voice channel')
+    await interaction.response.send_message(f'**Stopped and disconnected from voice channel {voice_client.channel}**')
+
+@bot.tree.command(name='play')
+async def youtubedlplay(interaction: discord.Interaction, link: str):
+    ffmpeg_options = {
+        'executable': r'C:\Users\thegoldendoge\Downloads\ffmpeg-n4.4-latest-win64-gpl-4.4\ffmpeg-n4.4-latest-win64-gpl-4.4\bin\ffmpeg.exe',
+    }
+    voice_client = interaction.guild.voice_client
+    await interaction.response.defer()
+
+
+    if not voice_client:
+        channel = interaction.user.voice.channel
+        voice_client = await channel.connect()
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'outtmpl': f'song.mp3',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192', }]
+    }
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(link, download=False)
+        linkydl = info['formats'][-20]['url']
+    if voice_client.is_playing():
+        voice_client.stop()
+    voice_client.play(discord.FFmpegPCMAudio(source=linkydl, **ffmpeg_options))
+    await interaction.followup.send(f'**Now playing from YouTube:** {info["title"]}\n**From link:** {link}')
 
 bot.run(token)
