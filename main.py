4f21cadb4b60814163fb7f7639a9aa78d2135a2d
thegoldendoge1 <thegoldendoge1@gmail.com>
2023-05-26 19:06:47 +0700

    added slash commands ^_^

--- main.py
+++ main.py
@@ -1,64 +1,46 @@
 import discord
+from discord import app_commands
 from discord.ext import commands
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
+import youtube_dl
 
-# Токены и пороли
-SPOTIFY_CLIENT_ID = ''
-SPOTIFY_CLIENT_SECRET = ''
+# Токены
 token = ''
-# Интенты дискорд
 
-intents = discord.Intents.default()
-intents.voice_states = True
-intents.message_content = True
+# Все разрешения интентов
+intents = discord.Intents.all()
 # Инициализация
-sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id = SPOTIFY_CLIENT_ID, client_secret = SPOTIFY_CLIENT_SECRET))
 bot = commands.Bot(command_prefix=">", intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f'Logged in as {bot.user.name}')
+    print(f'**logged in as {bot.user.name}**')
+    try:
+        synced = await bot.tree.sync()
+        print(f'** synced {len(synced)} command(s)')
+    except Exception as e:
+        print(e)
+@bot.tree.command(name='test')
+async def hello(interaction: discord.Interaction):
+    await interaction.response.send_message(f'test command succesfully! {interaction.user.mention}')
 
-@bot.command()
-async def play(ctx, url):
-    ffmpeg_path = r'C:\Users\thegoldendoge\Downloads\ffmpeg-n6.0-latest-win64-lgpl-shared-6.0\ffmpeg-n6.0-latest-win64-lgpl-shared-6.0\bin\ffmpeg.exe'
-    channel = ctx.author.voice.channel
-
-    if not channel:
-        await ctx.send("You are not connected to a voice channel.")
-        return
-
-    voice_client = ctx.voice_client
-
-    if voice_client and voice_client.is_playing():
-        voice_client.stop()
-
-    if not voice_client or not voice_client.is_connected():
+@bot.tree.command(name='play')
+async def play(interaction: discord.Interaction, url: str):
+    ffmpeg_path = r'C:\Users\thegoldendoge\Downloads\ffmpeg-2023-05-22-git-877ccaf776-full_build\bin\ffmpeg.exe'
+    channel = interaction.user.voice.channel
+    voice_client = interaction.guild.voice_client
+    if voice_client is None:
         voice_client = await channel.connect()
-        await ctx.send(f'** connected to {channel} **')
+    else:
+        await voice_client.move_to(channel)
+
     audio_source = discord.FFmpegPCMAudio(url, executable=ffmpeg_path)
     voice_client.play(audio_source)
-    await ctx.send("**playing...**")
 
-@bot.command()
-async def stop(ctx):
-    voice_client = ctx.voice_client
-
-    if voice_client and voice_client.is_playing():
-        voice_client.stop()
-        await ctx.send("**playback stopped :(**")
-    else:
-        await ctx.send("**no audio is currently playing**")
-
-@bot.command()
-async def disconnect(ctx):
-    voice_client = ctx.voice_client
-
-    if voice_client:
-        await voice_client.disconnect()
-        await ctx.send("**bot disconnected from the voice channel**")
-    else:
-        await ctx.send("**bot is not connected to a voice channel**")
+    await interaction.response.send_message('Playing song from url')
+@bot.tree.command(name='stop', description='Stop playing current song')
+async def stop(interaction: discord.Interaction):
+    voice_client = interaction.guild.voice_client
+    await voice_client.disconnect()
+    await interaction.response.send_message('Stopped and disconnected from voice channel')
 
 bot.run(token)
